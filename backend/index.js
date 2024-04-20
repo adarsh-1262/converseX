42f8909742dcdd9a955a7c8aeb513d4f83cbf15c
ADARSH SINGH <9792051262a@gmail.com>
2024-04-20 23:44:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,3 @@
-// const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database.js';
@@ -16,24 +15,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-// const corsOption = {
-//     origin: 'https://converse-x.vercel.app',
-//     credentials: true,
-//     optionSuccessStatus: 200,
-// };
-// Define your custom CORS middleware function
-const customCorsMiddleware = cors({
-  origin: 'https://converse-x.vercel.app', // Allow requests from this origin
-  credentials: true, // Allow sending cookies
-  methods: ['GET', 'POST', 'PUT'], // Specify the allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify the allowed headers
-});
-app.use(cors(customCorsMiddleware));
-// CORS configuration
-// app.use(cors({
-//     origin: 'https://converse-x.vercel.app',
-//     credentials: true,
-// }));
+
+// CORS configuration using the cors() function directly
+app.use(cors({
+    origin: 'https://converse-x.vercel.app',
+    credentials: true,
+}));
+
 
 // routes
 app.use("/api/v1/user", userRoute);
