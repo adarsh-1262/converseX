48c08cd15327953aab7bc36c25f7983f492e7000
ADARSH SINGH <9792051262a@gmail.com>
2024-04-22 00:20:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://converse-x.vercel.app'
-    }
-  }
   plugins: [react()],
 })
