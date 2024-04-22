96cf5f450eecb65853c367480670a86cde4833fd
ADARSH SINGH <9792051262a@gmail.com>
2024-04-22 11:31:26 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ['https://converse-x.vercel.app/'],
+        origin: ['https://converse-x.vercel.app'],
         methods: ["GET", "POST"],
     },
 });
