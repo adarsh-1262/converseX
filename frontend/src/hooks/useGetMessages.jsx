e557bfab6ac40d1a356614785ecaa1f375853b20
ADARSH SINGH <9792051262a@gmail.com>
2024-04-22 11:17:14 +0530

    Update useGetMessages.jsx

--- frontend/src/hooks/useGetMessages.jsx
+++ frontend/src/hooks/useGetMessages.jsx
@@ -10,7 +10,7 @@ function useGetMessages() {
         const fetchMessages = async () => {
             try {
                 axios.defaults.withCredentials = true;
-                const res = await axios.get(`/api/v1/message/${selectedUser?._id}`);
+                const res = await axios.get(`https://converse-x-backend.vercel.app/api/v1/message/${selectedUser?._id}`);
                 
                 dispatch(setMessages(res.data));
 
